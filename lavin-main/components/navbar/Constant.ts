8af8404ac4311b685bc99fb203d01fa4b058ab0f
LAV1N <156920217+LAV1N@users.noreply.github.com>
2024-07-14 21:09:23 +0700

    2.1.0

---
+++ lavin-main/components/navbar/Constant.ts
@@ -0,0 +1,27 @@
+export type NavLink = {
+  url: string;
+  text: string;
+};
+
+export const links: NavLink[] = [
+  // {
+  //   text: "Blog",
+  //   url: "/blog",
+  // },
+  {
+    text: "Projects",
+    url: "/projects",
+  },
+  // {
+  //   text: "Snippets",
+  //   url: "/snippet",
+  // },
+  {
+    text: "Github",
+    url: "https://github.com/LAV1N",
+  },
+  {
+    text: "About",
+    url: "/about",
+  },
+];
---
+++ lavin-main/data/socials.ts
@@ -0,0 +1,19 @@
+export default [
+  {
+    name: "Email",
+    url: "radityaprtama@gmail.com",
+  },
+  {
+    name: "Github",
+    url: "https://github.com/LAV1N",
+  },
+  {
+    name: "Instagram",
+    url: "https://instagram.com/unsqin_",
+  },
+  {
+    name: "Spotify",
+    url: "https://open.spotify.com/user/jq8jqs2zl5wrff86ko6ep9e04?si=d0b2d1bb0a1e4e78",
+  },
+  
+];
