38f23d8b4d88e1d3e0c27e57f4c17a1ea71277e0
LAV1N <156920217+LAV1N@users.noreply.github.com>
2024-07-18 16:58:16 +0700

    Update SEO

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export default {
     process.env.NEXT_PUBLIC_BASE_URL ||
     (process.env.NEXT_PUBLIC_VERCEL_URL &&
       `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) ||
-    "https://lavin-lake.vercel.app/",
+    "https://pratama-eta.vercel.app/",
   description:
     "Personal website of Raditya Alia Pratama. A passionate software engineer that focused on the web platform.",
 };
